6bc7bf677baf63b0264a39bf80fab127438095db
Vitaliy Kamelin <v.kamelin@gmail.com>
2025-08-24 18:49:57 +0500

    feat(app/Telegram): Добавление метода для получения ID пользователя (issue: )
    Добавлен метод `getUserId` для получения ID пользователя из различных типов обновлений Telegram.
    Метод ищет пользователя в различных объектах обновления (сообщения, отредактированные сообщения, каналы и т.д.).
    Возвращает `null`, если пользователь не найден.
    Также обработана возможность получения ID пользователя из реакции на сообщение.

--- app/Telegram/UpdateHelper.php
+++ app/Telegram/UpdateHelper.php
@@ -46,5 +46,43 @@ final class UpdateHelper
     {
         return $update->getRawData()['removed_chat_boost'] ?? null;
     }
+    
+    public static function getUserId(Update $update): ?int
+    {
+        $sources = [
+            ['method' => 'getMessage', 'getFrom' => true],
+            ['method' => 'getEditedMessage', 'getFrom' => true],
+            ['method' => 'getChannelPost', 'getFrom' => true],
+            ['method' => 'getEditedChannelPost', 'getFrom' => true],
+            ['method' => 'getInlineQuery', 'getFrom' => true],
+            ['method' => 'getChosenInlineResult', 'getFrom' => true],
+            ['method' => 'getCallbackQuery', 'getFrom' => true],
+            ['method' => 'getShippingQuery', 'getFrom' => true],
+            ['method' => 'getPreCheckoutQuery', 'getFrom' => true],
+            ['method' => 'getPollAnswer', 'getUser' => true],
+            ['method' => 'getMyChatMember', 'getFrom' => true],
+            ['method' => 'getChatMember', 'getFrom' => true],
+            ['method' => 'getChatJoinRequest', 'getFrom' => true],
+        ];
+        
+        foreach ($sources as $source) {
+            $object = $update->{$source['method']}();
+            if ($object) {
+                if (($source['getFrom'] ?? false) && $object->getFrom()) {
+                    return (int)$object->getFrom()->getId();
+                }
+                if (($source['getUser'] ?? false) && $object->getUser()) {
+                    return (int)$object->getUser()->getId();
+                }
+            }
+        }
+        
+        $reaction = self::getMessageReaction($update);
+        if ($reaction !== null && isset($reaction['user']['id'])) {
+            return (int)$reaction['user']['id'];
+        }
+        
+        return null;
+    }
 }
 
