7c6d313d1c57070ab24c705b734d89b754062f40
Vitaliy Kamelin <vkamelin@users.noreply.github.com>
2025-08-26 22:04:02 +0500

    feat: add file datatable with download action

--- public/assets/js/datatable.files.js
+++ public/assets/js/datatable.files.js
@@ -1,16 +1,20 @@
 $(document).ready(function() {
   createDatatable('#filesTable', '/dashboard/files/data', [
     { data: 'id' },
-    { data: 'type' },
     { data: 'original_name' },
-    { data: 'mime_type' },
-    { data: 'size' },
+    { data: 'type' },
+    { data: 'file_id' },
     { data: 'created_at' },
     {
       data: null,
       className: 'text-end',
       render: function(data, type, row) {
-        return '<a href="/dashboard/files/' + row.id + '" class="btn btn-sm btn-outline-secondary">View</a>';
+        return '<a href="/dashboard/files/' + row.id + '" class="btn btn-sm btn-outline-secondary me-1">'
+          + '<i class="bi bi-eye" title="Просмотр"></i>'
+          + '</a>'
+          + '<a href="/dashboard/files/' + row.id + '/download" class="btn btn-sm btn-outline-secondary">'
+          + '<i class="bi bi-download" title="Скачать"></i>'
+          + '</a>';
       },
       orderable: false,
       searchable: false
